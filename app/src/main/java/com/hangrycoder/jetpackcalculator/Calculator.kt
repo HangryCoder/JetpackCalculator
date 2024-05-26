f4b1f2ccff4c90d8920cba99d0541033361b9c48
Sonia Wadji <soniawadji@gmail.com>
2024-05-26 12:44:06 +0530

    Moved calculate logic to Calculator class. Still WIP

--- app/src/main/java/com/hangrycoder/jetpackcalculator/Calculator.kt
+++ app/src/main/java/com/hangrycoder/jetpackcalculator/Calculator.kt
@@ -21,4 +21,34 @@ class Calculator {
     fun percentage(firstNum: Double, secondNum: Double): Double {
         return firstNum % secondNum
     }
+
+    fun calculate(firstNum: Double, secondNum: Double, operatorId: Int): Double {
+        when (operatorId) {
+            Operation.Percentage.value -> {
+                return percentage(firstNum, secondNum)
+            }
+
+            Operation.Divide.value -> {
+                return divide(firstNum, secondNum)
+            }
+
+            Operation.Multiply.value -> {
+                return multiply(firstNum, secondNum)
+            }
+
+            Operation.Subtract.value -> {
+                return subtract(firstNum, secondNum)
+            }
+
+            Operation.Addition.value -> {
+                return add(firstNum, secondNum)
+            }
+
+            else -> {
+                //Throw an error
+                // _calculation.value = "Error"
+                return 0.0
+            }
+        }
+    }
 }
--- app/src/main/java/com/hangrycoder/jetpackcalculator/CalculatorViewModel.kt
+++ app/src/main/java/com/hangrycoder/jetpackcalculator/CalculatorViewModel.kt
@@ -146,7 +146,6 @@ class CalculatorViewModel : ViewModel() {
                 val calculator = Calculator()
                 when (operatorId) {
                     Operation.Percentage.value -> {
-                        //result %= secondNumber
                         result = calculator.percentage(result, secondNumber)
                     }
 
