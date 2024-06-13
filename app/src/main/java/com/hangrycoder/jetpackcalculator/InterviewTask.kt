d5cf9d4b41d004d904ce314a27dee796fc931c54
Sonia Wadji <soniawadji@gmail.com>
2024-06-13 15:48:56 +0530

    Cleaned up some code

--- app/src/main/java/com/hangrycoder/jetpackcalculator/InterviewTask.kt
+++ app/src/main/java/com/hangrycoder/jetpackcalculator/InterviewTask.kt
@@ -3,12 +3,8 @@ package com.hangrycoder.jetpackcalculator
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.async
-import kotlinx.coroutines.cancelAndJoin
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.supervisorScope
